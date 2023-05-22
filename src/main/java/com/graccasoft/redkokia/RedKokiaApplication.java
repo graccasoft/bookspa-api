edb4c470e9e5a73161c79650c18ebc4cfe877825
Gracious Mashasha <graccasoft263@gmail.com>
2023-05-22 16:19:49 +0200

    initial

---
+++ src/main/java/com/graccasoft/redkokia/RedKokiaApplication.java
@@ -0,0 +1,13 @@
+package com.graccasoft.redkokia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedKokiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedKokiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graccasoft/redkokia/RedKokiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graccasoft.redkokia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedKokiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
